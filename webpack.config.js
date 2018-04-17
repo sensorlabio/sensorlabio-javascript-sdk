9249e7f5c2ea4267335a9383a82d150e219a84be
Gray Hound <admin@devilmaydie.name>
2018-04-17 16:09:04 +0500

    Correct package name

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.min.js',
         libraryTarget: 'umd',
-        library: 'sensorlab-javascript-sdk'
+        library: 'sensorlabio-javascript-sdk'
     },
     module: {
         loaders: [
