28c0d45dbcb1557ab0463a28416cec23fa5b96bb
Gray Hound <admin@devilmaydie.name>
2018-08-28 17:03:46 +0500

    WS path

--- src/websockets.js
+++ src/websockets.js
@@ -12,7 +12,7 @@ export default class SensorlabWebsocket {
     async connect(token, sensor) {
         return new Promise((resolve, reject) => {
             //connect
-            this.socket = io.connect(this.url, { path: 'chat/socket.io' });
+            this.socket = io.connect(this.url, { path: 'ws/socket.io' });
 
             //try to authenticate on connection
             this.socket.on('connect', () => {
