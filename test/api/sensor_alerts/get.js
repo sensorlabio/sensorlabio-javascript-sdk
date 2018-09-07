0064452bf36c97e4a1418218da8d2054acc8af5f
Gray Hound <admin@devilmaydie.name>
2018-09-07 11:22:29 +0500

    Tests for sensor alerts - get

--- test/api/sensor_alerts/get.js
+++ test/api/sensor_alerts/get.js
@@ -1,20 +1,21 @@
-let chai = require('chai');
-let should = chai.should();
-let expect = chai.expect;
 import {SensorlabApi} from '../../../src';
 
-//@todo change url to real public test server
+let uuid = require('uuid');
+
 let api = new SensorlabApi(process.env.TEST_REST_API_URL); //we must test on test server only
 
 let test_email = 'test@sensorlab.io';
 let test_passw = 'test';
 
-let first_app = null;
+let sensor_1 = null;
+let sensor_2 = null;
+let sensor_alert_1 = null;
+let sensor_alert_2 = null;
 
-describe('Applications endpoints', () => {
-    describe('GET /applications/:id', () => {
+describe('Sensor alerts configuration endpoints', () => {
+    describe('Get sensor alert', () => {
         it('should get an 401 status error without authorization', (done) => {
-            api.applications.get()
+            api.sensor_alerts.get()
                 .catch((response) => {
                     response.status.should.eq(401);
                     done();
@@ -29,30 +30,83 @@ describe('Applications endpoints', () => {
                 });
         });
 
-        it('should get list of sensors default page=1', (done) => {
-            api.applications.list()
+        it('should get list of sensors and save one for testing', (done) => {
+            api.sensors.list()
                 .then((response) => {
-                    response.applications.should.be.a('array').lengthOf(50);
-                    response.should.have.property('count');
-                    response.should.have.property('pages');
-                    first_app = response.applications[0];
+                    sensor_1 = response.sensors[2];
+                    sensor_2 = response.sensors[3];
                     done();
                 });
         });
 
-        it('should get app', (done) => {
-            api.applications.get(first_app.id)
-                .then((response) => {
-                    response.should.have.property('id').eq(first_app.id);
-                    response.should.have.property('name').eq(first_app.name);
-                    response.should.have.property('description').eq(first_app.description);
-                    response.should.have.property('created');
+        it('should create new sensor alert configuration without problem', (done) => {
+            api.sensor_alerts.create(sensor_1.id, 'max', 'TMP', 0)
+                .then((new_sensor_alert) => {
+                    new_sensor_alert.should.be.a('object');
+                    new_sensor_alert.should.have.property('id');
+                    new_sensor_alert.should.have.property('threshold_type').eq('max');
+                    new_sensor_alert.should.have.property('measurement_type').eq('TMP');
+                    new_sensor_alert.should.have.property('threshold_value').eq(0);
+                    sensor_alert_1 = new_sensor_alert;
                     done();
                 });
         });
 
-        it('should get 404 error on uknown app', (done) => {
-            api.applications.get('someid')
+        it('should create new sensor alert configuration without problem', (done) => {
+            api.sensor_alerts.create(sensor_2.id, 'loc', 'LOC', '36.1812440939046,-101.84828589116029,1000')
+                .then((new_sensor_alert) => {
+                    new_sensor_alert.should.be.a('object');
+                    new_sensor_alert.should.have.property('id');
+                    new_sensor_alert.should.have.property('threshold_type').eq('loc');
+                    new_sensor_alert.should.have.property('measurement_type').eq('LOC');
+                    new_sensor_alert.should.have.property('threshold_value').containSubset({lat: 36.1812440939046, lng: -101.84828589116029,radius: 1000});
+                    sensor_alert_2 = new_sensor_alert;
+                    done();
+                });
+        });
+
+        it('should get first sensor alert config by id', (done) => {
+            api.sensor_alerts.get(sensor_1.id, sensor_alert_1.id)
+                .then((sensor_alert) => {
+                    sensor_alert.should.be.a('object');
+                    sensor_alert.should.have.property('id');
+                    sensor_alert.should.have.property('threshold_type').eq('max');
+                    sensor_alert.should.have.property('measurement_type').eq('TMP');
+                    sensor_alert.should.have.property('threshold_value').eq(0);
+                    done();
+                });
+        });
+
+        it('should get first sensor alert config by id', (done) => {
+            api.sensor_alerts.get(sensor_2.id, sensor_alert_2.id)
+                .then((sensor_alert) => {
+                    sensor_alert.should.be.a('object');
+                    sensor_alert.should.have.property('id');
+                    sensor_alert.should.have.property('threshold_type').eq('loc');
+                    sensor_alert.should.have.property('measurement_type').eq('LOC');
+                    sensor_alert.should.have.property('threshold_value').containSubset({lat: 36.1812440939046, lng: -101.84828589116029,radius: 1000});
+                    done();
+                });
+        });
+
+        it('should get 404 error on uknown sensor alert', (done) => {
+            api.applications.get(sensor_1, uuid.v1())
+                .catch((response) => {
+                    response.status.should.eq(404);
+                    done();
+                });
+        });
+
+        it('should get first sensor alert config by id', (done) => {
+            api.sensor_alerts.get(sensor_1.id, sensor_alert_2.id)
+                .catch((response) => {
+                    response.status.should.eq(404);
+                    done();
+                });
+        });
+
+        it('should get first sensor alert config by id', (done) => {
+            api.sensor_alerts.get(sensor_2.id, sensor_alert_1.id)
                 .catch((response) => {
                     response.status.should.eq(404);
                     done();
