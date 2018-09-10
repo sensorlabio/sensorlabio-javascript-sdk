8cf034ea80c4fab27a9cbe13de88d0c365751335
Gray Hound <admin@devilmaydie.name>
2018-09-10 18:26:01 +0500

    small JSDoc changes

--- src/endpoints/sensor_alerts.js
+++ src/endpoints/sensor_alerts.js
@@ -48,7 +48,7 @@ export default class SensorAlertsEndpoints {
      * @method SensorAlertsEndpoints#create
      * @param {string} threshold_type - Threshold type
      * @param {string} measurement_type - Measurement type
-     * @param {string} threshold_value - Threshold value
+     * @param {string|number|Object} threshold_value - Threshold value
      */
     async create(sensor, threshold_type, measurement_type, threshold_value) {
         let data = {
@@ -68,7 +68,7 @@ export default class SensorAlertsEndpoints {
      * @param {string} alert - Sensor Alert ID
      * @param {string} threshold_type - Threshold type
      * @param {string} measurement_type - Measurement type
-     * @param {string} threshold_value - Threshold value
+     * @param {string|number|Object} threshold_value - Threshold value
      */
     async update(sensor, alert, threshold_type, measurement_type, threshold_value) {
         let data = {
