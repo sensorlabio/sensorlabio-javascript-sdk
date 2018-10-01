a5cb5dbcdf5ae995a665e20ff7dca777573613d4
Gray Hound <admin@devilmaydie.name>
2018-10-01 16:38:51 +0500

    Send `public_api_key` for /public namespace

--- src/websockets/public.js
+++ src/websockets/public.js
@@ -1,5 +1,7 @@
 import BasicWebsocket from "./basic";
 
+let io = require('socket.io-client');
+
 export default class SensorlabPublicWebsocket extends BasicWebsocket {
     constructor(websocket_url = 'http://staging.sensorlab.io') {
         super(websocket_url);
@@ -7,6 +9,31 @@ export default class SensorlabPublicWebsocket extends BasicWebsocket {
 
         //we must force a new connection since because this namespace has different authentication method
         this.force_new = true;
+    }
+
+    async connect(public_api_key) {
+        return new Promise((resolve, reject) => {
+            //connect
+            this.socket = io(this.url + this.namespace + '?public_api_key='+public_api_key, {
+                path: '/ws',
+                forceNew: this.force_new
+            });
+
+            //try to authenticate on connection
+            this.socket.on('connect', () => {
+                this._onConnected();
+                resolve();
+            });
+
+            this.socket.on('error', function (reason){
+                console.error('Unable to connect Socket.IO', reason);
+            });
+
+            //catch disconnect
+            this.socket.on('disconnect', () => {
+                this._onDisconnect();
+            });
+        });
     }
 
     joinSensor(sensor, type = null) {
