0eb67ef1d67e5d5b17c7c1d8835124e7333326da
Gray Hound <admin@devilmaydie.name>
2018-10-04 17:34:45 +0500

    Added documentation on sensor alerts

---
+++ src/responses/sensor_alerts.js
@@ -0,0 +1,29 @@
+import SensorAlert from '../models/sensor_alert';
+
+/**
+ * Response with sensor alerts data
+ */
+export default class SensorAlertsResponse {
+    /**
+     * @constructor SensorAlertsResponse
+     *
+     * @param {SensorlabApi} api - parent api
+     * @param {Object} data - data from response
+     */
+    constructor(api, data) {
+        this.api = api;
+
+        /**
+         * List of sensors.
+         *
+         * @member SensorAlertsResponse#sensor_alerts
+         * @type {SensorAlert[]}
+         */
+        this.sensor_alerts = [];
+
+        let self = this;
+        data.forEach(function(alert_data) {
+            self.sensor_alerts.push(new SensorAlert(self.api, alert_data));
+        });
+    }
+}
