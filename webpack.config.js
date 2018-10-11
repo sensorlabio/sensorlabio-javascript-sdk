8f49771e94859d682fa27a40dd5b00bd96680f45
Gray Hound <admin@devilmaydie.name>
2018-10-11 15:54:30 +0500

    Different builds for node and web

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
-var path = require('path');
-var webpack = require('webpack');
+let path = require('path');
+let webpack = require('webpack');
 
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-module.exports = {
+let UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+let webConfig = {
     target: 'web',
     entry: ['babel-polyfill', './src/index.js'],
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'web.min.js',
+        libraryTarget: 'umd',
+        library: 'SensorlabSDK'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    /(node_modules|bower_components|test|build)/,
+                    path.resolve(__dirname, 'index.js'),
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            }
+        ]
+    },
+    plugins: [new UglifyJsPlugin({
+        compressor: {
+            warnings: false
+        }
+    })]
+}
+
+let nodeConfig = {
+    target: 'web',
+    entry: ['./src/index.js'],
     devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'build'),
@@ -32,4 +65,6 @@ module.exports = {
             warnings: false
         }
     })]
-};
+}
+
+module.exports = [ webConfig, nodeConfig ];
