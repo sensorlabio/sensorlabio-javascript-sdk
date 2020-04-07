6a19f2a11cce298c6799632379cd67513cc9835d
Gray Hound <admin@devilmaydie.name>
2020-04-07 14:05:28 +0500

    removing outdated `module.exports`, using only `exports`. Bumping version to 2.0.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import SensorlabApi from './api';
 import SensorlabWebsockets from './websockets';
 
-module.exports = {
-    'SensorlabApi': SensorlabApi,
-    'SensorlabWebsockets': SensorlabWebsockets,
-}
+export {SensorlabApi, SensorlabWebsockets};
