98d56893b8ac307ef447fdfe297f89a000592dfa
Gray Hound <admin@devilmaydie.name>
2020-04-20 16:02:21 +0500

    Owner field for sensor

--- src/models/sensor.js
+++ src/models/sensor.js
@@ -2,112 +2,120 @@
  * Sensor model
  */
 export default class Sensor {
+  /**
+   * @constructor Sensor
+   * @param {SensorlabApi} api - parent api
+   * @param {Object} data - data from response
+   */
+  constructor(api, data) {
+    this.api = api;
+
     /**
-     * @constructor Sensor
-     * @param {SensorlabApi} api - parent api
-     * @param {Object} data - data from response
+     * Sensor ID.
+     *
+     * @member Sensor#id
+     * @type {string}
      */
-    constructor(api, data) {
-        this.api = api;
+    this.id = data.id;
 
-        /**
-         * Sensor ID.
-         *
-         * @member Sensor#id
-         * @type {string}
-         */
-        this.id = data.id;
+    /**
+     * Sensor IMEI.
+     *
+     * @member Sensor#imei
+     * @type {string}
+     */
+    this.imei = data.imei;
 
-        /**
-         * Sensor IMEI.
-         *
-         * @member Sensor#imei
-         * @type {string}
-         */
-        this.imei = data.imei;
+    /**
+     * Sensor name.
+     *
+     * @member Sensor#name
+     * @type {string}
+     */
+    this.name = data.name;
 
-        /**
-         * Sensor name.
-         *
-         * @member Sensor#name
-         * @type {string}
-         */
-        this.name = data.name;
+    /**
+     * Application ID.
+     *
+     * @member Sensor#applications
+     * @type {string}
+     */
+    this.applications = data.applications;
 
-        /**
-         * Application ID.
-         *
-         * @member Sensor#applications
-         * @type {string}
-         */
-        this.applications = data.applications;
+    /**
+     * Owner.
+     *
+     * @member Sensor#owner
+     * @type {Object.owner|null}
+     */
+    this.owner = data.owner;
 
-        /**
-         * Date/time created.
-         *
-         * @member Sensor#created
-         * @type {*}
-         */
-        this.created = data.created;
+    /**
+     * Date/time created.
+     *
+     * @member Sensor#created
+     * @type {*}
+     */
+    this.created = data.created;
 
-        /**
-         * Battery charge.
-         *
-         * @member Sensor#batteryCharge
-         * @type {number}
-         */
-        this.batteryCharge = data.batteryCharge;
+    /**
+     * Battery charge.
+     *
+     * @member Sensor#batteryCharge
+     * @type {number}
+     */
+    this.batteryCharge = data.batteryCharge;
 
-        /**
-         * Is battery charging?
-         *
-         * @member Sensor#isBatteryCharging
-         * @type {boolean}
-         */
-        this.isBatteryCharging = data.isBatteryCharging;
+    /**
+     * Is battery charging?
+     *
+     * @member Sensor#isBatteryCharging
+     * @type {boolean}
+     */
+    this.isBatteryCharging = data.isBatteryCharging;
 
-        /**
-         * Is sensor connected and online?
-         *
-         * @member Sensor#isOnline
-         * @type {boolean}
-         */
-        this.isOnline = data.isOnline;
+    /**
+     * Is sensor connected and online?
+     *
+     * @member Sensor#isOnline
+     * @type {boolean}
+     */
+    this.isOnline = data.isOnline;
 
-        /**
-         * Is sensor public?
-         *
-         * @member Sensor#is_public
-         * @type {boolean}
-         */
-        this.is_public = data.is_public;
+    /**
+     * Is sensor public?
+     *
+     * @member Sensor#is_public
+     * @type {boolean}
+     */
+    this.is_public = data.is_public;
 
-        this.token = data.token;
+    this.token = data.token;
 
-        let self = this;
+    let self = this;
 
-        /**
-         * Access to measurments methods for current sensor.
-         *
-         * @member Sensor#measurements
-         * @type {Object}
-         */
-        this.measurements = {
-            'list': function(options) {
-                if (options === undefined) {
-                    options = {};
-                }
-                options['sensor'] = self.id;
-                return self.api.measurements.list(options);
-            },
-            'last': function(options) {
-                if (options === undefined) {
-                    options = {};
-                }
-                options['sensor'] = self.id;
-                return self.api.measurements.last(options);
-            }
+    /**
+     * Access to measurments methods for current sensor.
+     *
+     * @member Sensor#measurements
+     * @type {Object}
+     */
+    this.measurements = {
+      'list': function (options) {
+        if (options === undefined) {
+          options = {};
         }
-
+        options['sensor'] = self.id;
+        return self.api.measurements.list(options);
+      },
+      'last': function (options) {
+        if (options === undefined) {
+          options = {};
+        }
+        options['sensor'] = self.id;
+        return self.api.measurements.last(options);
+      }
     }
+
+  }
 }
