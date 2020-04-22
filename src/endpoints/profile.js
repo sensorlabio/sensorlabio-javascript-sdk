2ca5684749ae370d6490638fb9dee7d572750e2e
Gray Hound <admin@devilmaydie.name>
2020-04-22 14:16:12 +0500

    Dashboard settings

--- src/endpoints/profile.js
+++ src/endpoints/profile.js
@@ -4,58 +4,75 @@ import Profile from '../models/profile';
  * Class for /profile/* endpoints.
  */
 export default class ProfileEndpoints {
+  /**
+   * @constructor ProfileEndpoints
+   * @param {SensorlabApi} api - parent api
+   */
+  constructor(api) {
     /**
-     * @constructor ProfileEndpoints
-     * @param {SensorlabApi} api - parent api
+     * @member ProfileEndpoints#api
+     * @type {SensorlabApi}
      */
-    constructor(api) {
-        /**
-         * @member ProfileEndpoints#api
-         * @type {SensorlabApi}
-         */
-        this.api = api;
-    }
+    this.api = api;
+  }
 
-    /**
-     * Get user's profile.
-     *
-     * @method ProfileEndpoints#get
-     * @returns {Promise.<ApiResponse>}
-     */
-    async get() {
-        let response = await this.api._makeApiRequest('/v1/profile', 'GET', null, null, true);
-        return this.api._prepareApiResponse(response, this._successProfileResult);
+  /**
+   * Get user's profile.
+   *
+   * @method ProfileEndpoints#get
+   * @returns {Promise.<ApiResponse>}
+   */
+  async get() {
+    let response = await this.api._makeApiRequest('/v1/profile', 'GET', null, null, true);
+    return this.api._prepareApiResponse(response, this._successProfileResult);
 
-    }
+  }
 
-    /**
-     * Change password.
-     *
-     * @method ProfileEndpoints#change_password
-     * @param {string} old_password - current (old) user's password
-     * @param {string} new_password - new password to set
-     * @param {string} new_password_check - new password check
-     * @returns {Promise.<ApiResponse>}
-     */
-    async change_password(old_password, new_password, new_password_check) {
-        let data = {
-            'old_password': old_password,
-            'new_password': new_password,
-            'new_password_check': new_password_check,
-        };
-        let response = await this.api._makeApiRequest('/v1/profile/change_password', 'POST', data, null, true);
-        return this.api._prepareApiResponse(response);
-    }
+  /**
+   * Change password.
+   *
+   * @method ProfileEndpoints#change_password
+   * @param {string} old_password - current (old) user's password
+   * @param {string} new_password - new password to set
+   * @param {string} new_password_check - new password check
+   * @returns {Promise.<ApiResponse>}
+   */
+  async change_password(old_password, new_password, new_password_check) {
+    let data = {
+      'old_password': old_password,
+      'new_password': new_password,
+      'new_password_check': new_password_check,
+    };
+    let response = await this.api._makeApiRequest('/v1/profile/change_password', 'POST', data, null, true);
+    return this.api._prepareApiResponse(response);
+  }
 
-    /**
-     * Return success result.
-     *
-     * @param {SensorlabApi} api
-     * @param {object} response
-     * @returns {Profile}
-     * @private
-     */
-    _successProfileResult(api, response) {
-        return new Profile(api, response.data);
-    }
+  /**
+   * Dashboard settings.
+   *
+   * @method ProfileEndpoints#dashboard_settings
+   * @param {string} dashboardApplication - default dashboard application
+   * @param {string} dashboardMeasurementType - default measurement type
+   * @returns {Promise.<ApiResponse>}
+   */
+  async dashboard_settings(dashboardApplication, dashboardMeasurementType) {
+    let data = {
+      'dashboardApplication': dashboardApplication,
+      'dashboardMeasurementType': dashboardMeasurementType
+    };
+    let response = await this.api._makeApiRequest('/v1/profile/change_password', 'POST', data, null, true);
+    return this.api._prepareApiResponse(response);
+  }
+
+  /**
+   * Return success result.
+   *
+   * @param {SensorlabApi} api
+   * @param {object} response
+   * @returns {Profile}
+   * @private
+   */
+  _successProfileResult(api, response) {
+    return new Profile(api, response.data);
+  }
 }
--- src/models/profile.js
+++ src/models/profile.js
@@ -2,41 +2,69 @@
  * User profile
  */
 export default class Profile {
-    /**
-     * @constructor Profile
-     * @param {SensorlabApi} api - parent api
-     * @param {Object} data - data from response
-     */
-    constructor(api, data) {
-        this.api = api;
-
-        /**
-         * User's email
-         *
-         * @member Profile#email
-         * @type {string}
-         */
-        this.email = data.email;
-
-      /**
-       * Is Application Admin or not
-       *
-       * @member Profile#isApplicationAdmin
-       * @type {Boolean}
-       */
-        this.isApplicationAdmin = data.isApplicationAdmin;
-    }
+  /**
+   * @constructor Profile
+   * @param {SensorlabApi} api - parent api
+   * @param {Object} data - data from response
+   */
+  constructor(api, data) {
+    this.api = api;
 
     /**
-     * Change password for profile/user.
+     * User's email
      *
-     * @method Profile#change_password
-     * @param old_password
-     * @param new_password
-     * @param new_password_check
-     * @returns {Promise.<ApiResponse>}
+     * @member Profile#email
+     * @type {string}
      */
-    async change_password(old_password, new_password, new_password_check) {
-        return this.api.profile.change_password(old_password, new_password, new_password_check)
-    }
+    this.email = data.email;
+
+    /**
+     * Is Application Admin or not
+     *
+     * @member Profile#isApplicationAdmin
+     * @type {Boolean}
+     */
+    this.isApplicationAdmin = data.isApplicationAdmin;
+
+    /**
+     * Dashboard default application
+     *
+     * @member Profile#dashboardApplication
+     * @type {Object.dashboardApplication}
+     */
+    this.dashboardApplication = data.dashboardApplication;
+
+    /**
+     * Dashboard default measurement type
+     *
+     * @member Profile#dashboardMeasurementType
+     * @type {Object.dashboardMeasurementType}
+     */
+    this.dashboardMeasurementType = data.dashboardMeasurementType;
+  }
+
+  /**
+   * Change password for profile/user.
+   *
+   * @method Profile#change_password
+   * @param old_password
+   * @param new_password
+   * @param new_password_check
+   * @returns {Promise.<ApiResponse>}
+   */
+  async change_password(old_password, new_password, new_password_check) {
+    return this.api.profile.change_password(old_password, new_password, new_password_check);
+  }
+
+  /**
+   * Dashboard settings.
+   *
+   * @method ProfileEndpoints#dashboard_settings
+   * @param {string} dashboardApplication - default dashboard application
+   * @param {string} dashboardMeasurementType - default measurement type
+   * @returns {Promise.<ApiResponse>}
+   */
+  async dashboard_settings(dashboardApplication, dashboardMeasurementType) {
+    return this.api.profile.dashboard_settings(dashboardApplication, dashboardMeasurementType);
+  }
 }
