35e96027611b482cf4694846679e217a1dff5237
Maksim Jason Hound <admin@devilmaydie.name>
2020-06-17 20:18:52 +0500

    New build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,31 @@
 let path = require('path');
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
 let webConfig = {
-    target: 'web',
-    mode: 'production',
-    entry: ['@babel/polyfill', './src/index.js'],
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'web.min.js',
-        libraryTarget: 'umd',
-        library: 'SensorlabSDK'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [
-                    /(node_modules|bower_components|test|build)/,
-                    path.resolve(__dirname, 'index.js'),
-                ],
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        ]
-    },
+  target: 'web',
+  mode: 'production',
+  entry: ['@babel/polyfill', './src/index.js'],
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'web.min.js',
+    libraryTarget: 'umd',
+    library: 'SensorlabSDK'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          /(node_modules|bower_components|test|build)/,
+          path.resolve(__dirname, 'index.js'),
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  },
   /*
     optimization: {
         minimizer: [
@@ -38,40 +36,40 @@ let webConfig = {
 }
 
 let nodeConfig = {
-    target: 'node',
-    mode: 'production',
-    entry: ['./src/index.js'],
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.min.js',
-        libraryTarget: 'umd',
-        library: 'SensorlabSDK'
-    },
-    module: {
-        noParse: [/ws/],
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [
-                    /(node_modules|bower_components|test|build)/,
-                    path.resolve(__dirname, 'index.js'),
-                ],
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env']
-                }
-            }
+  target: 'node',
+  mode: 'production',
+  entry: ['./src/index.js'],
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.min.js',
+    libraryTarget: 'umd',
+    library: 'SensorlabSDK'
+  },
+  module: {
+    noParse: [/ws/],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          /(node_modules|bower_components|test|build)/,
+          path.resolve(__dirname, 'index.js'),
         ],
-    },
-    externals: ['ws'],
-    /*
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin()
-        ]
-    }
-    */
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ],
+  },
+  externals: ['ws'],
+  /*
+  optimization: {
+      minimizer: [
+          new UglifyJsPlugin()
+      ]
+  }
+  */
 }
 
-module.exports = [ webConfig, nodeConfig ];
+module.exports = [webConfig, nodeConfig];
