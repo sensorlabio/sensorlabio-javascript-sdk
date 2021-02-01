ab6559898af0c687c89866f3c311c7e24b060cd0
Gray Hound <admin@devilmaydie.name>
2021-02-02 00:16:53 +0500

    Add sensor to measurements

--- src/models/measurement.js
+++ src/models/measurement.js
@@ -2,54 +2,62 @@
  * @classdesc Measurement model
  */
 export default class Measurement {
-    /**
-     * @constructor Measurement
-     * @param {SensorlabApi} api - parent api
-     * @param {Object} data - data from response
-     */
-    constructor(api, data) {
-        this.api = api;
-
-        /**
-         * Measurement ID.
-         *
-         * @member Measurement#id
-         * @type {string}
-         */
-        this.id = data.id;
-
-        /**
-         * Measurement type.
-         *
-         * @member Measurement#type
-         * @type {string}
-         */
-        this.type = data.type;
-
-        /**
-         * Measurement values array.
-         *
-         * @member Measurement#value
-         * @type {array}
-         */
-        this.value = data.value;
-
-        /**
-         * Created date.
-         *
-         * @member Measurement#created
-         * @type {Date}
-         */
-        this.timestamp = data.timestamp;
-    }
+  /**
+   * @constructor Measurement
+   * @param {SensorlabApi} api - parent api
+   * @param {Object} data - data from response
+   */
+  constructor(api, data) {
+    this.api = api;
 
     /**
-     * Get sensor.
+     * Measurement ID.
      *
-     * @method Measurement#sensor
-     * @returns {Promise.<ApiResponse>}
+     * @member Measurement#id
+     * @type {string}
      */
-    async sensor() {
-        return this.api.sensors.one(this.sensor_id);
-    }
+    this.id = data.id;
+
+    /**
+     * Measurement type.
+     *
+     * @member Measurement#type
+     * @type {string}
+     */
+    this.type = data.type;
+
+    /**
+     * Measurement values array.
+     *
+     * @member Measurement#value
+     * @type {array}
+     */
+    this.value = data.value;
+
+    /**
+     * Created date.
+     *
+     * @member Measurement#timestamp
+     * @type {Date}
+     */
+    this.timestamp = data.timestamp;
+
+    /**
+     * Sensor.
+     *
+     * @member Measurement#sensor
+     * @type {String}
+     */
+    this.sensor = data.sensor;
+  }
+
+  /**
+   * Get sensor.
+   *
+   * @method Measurement#sensor
+   * @returns {Promise.<ApiResponse>}
+   */
+  async sensor() {
+    return this.api.sensors.one(this.sensor_id);
+  }
 }
